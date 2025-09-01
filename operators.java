e293d105257a765258e6566ee4a08e65eda9a468
elbaheleng <elbaheleng@gmail.com>
2025-09-01 16:10:59 +0530

    operators

---
+++ operators.java
@@ -0,0 +1,17 @@
+class operators {
+    public static void main(String[] args) {
+        int x = 6;
+        int y = 7;
+
+        int z = x * y;
+        System.out.println(z);
+
+        boolean result = x != y;
+        System.out.println(result);
+
+        int a = 7;
+        int b = 8;
+        boolean res = x < y && a < b;
+        System.out.println(res);
+    }
+}
