fbadaa1fbff8084ac9992922fed363615e8893ff
elbaheleng <elbaheleng@gmail.com>
2025-09-01 12:31:47 +0530

    variables

--- hello.java
+++ hello.java
@@ -1,7 +1,19 @@
 class hello
 {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        System.out.println("Hello World");
+        int num1 = 3, num2 = 5;
+        int result = num1 + num2;
+        System.out.println(result);
+        float num = 5.6f;
+        System.out.println(num);
+        char c = 'k';
+        System.out.println(c);
+        boolean b = true;
+        System.out.println(b);
+
+
+
     }
 
 }
