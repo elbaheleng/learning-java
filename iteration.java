69ebf5a97fddca734bd1a3de912730a837dbc260
elbaheleng <elbaheleng@gmail.com>
2025-09-09 11:49:03 +0530

    do while loop

--- iteration.java
+++ iteration.java
@@ -1,15 +1,23 @@
 public class iteration {
     public static void main(String[] args) {
-        int i =1;
-        while(i <= 4)
-        {
+        int i = 1;
+        while (i <= 4) {
             System.out.println("Hi " + i);
             int j = 1;
-            while(j<=3){ //nested loope
+            while (j <= 3) { // nested loope
                 System.out.println("Hello");
                 j++;
             }
             i++;
         }
+
+        // dowhile
+        System.out.println("Do while");
+        int k = 5;
+        do {
+            System.out.println("Hi " + k);
+            k++;
+        } while (k <= 4);
+
     }
 }
