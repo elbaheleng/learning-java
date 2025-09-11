db77cb11d59819f7c7e907fdf72c5499cc832517
elbaheleng <elbaheleng@gmail.com>
2025-09-11 12:01:58 +0530

    array of objects

--- array.java
+++ array.java
@@ -1,3 +1,9 @@
+class student {
+    int rollno;
+    String name;
+    int marks;
+}
+
 public class array {
     public static void main(String[] args) {
         int nums[][] = new int[3][4];
@@ -30,11 +36,35 @@ public class array {
                 jaggedarray[i][j] = (int) (Math.random() * 10);
             }
         }
-         for (int n[] : jaggedarray) {
+        for (int n[] : jaggedarray) {
             for (int m : n) {
                 System.out.print(m + " ");
             }
             System.out.println();
         }
+
+        // creating instances for student class
+        student s1 = new student();
+        student s2 = new student();
+        student s3 = new student();
+        s1.rollno = 1;
+        s1.name = "Elba";
+        s1.marks = 99;
+        s2.rollno = 2;
+        s2.name = "Emil";
+        s2.marks = 70;
+        s3.rollno = 3;
+        s3.name = "Meera";
+        s3.marks = 100;
+        // creating an array of type student
+        student studentsarray[] = new student[3];
+        studentsarray[0] = s1;
+        studentsarray[1] = s2;
+        studentsarray[2] = s3;
+        for(int i = 0; i<studentsarray.length; i++)
+        {
+            System.out.println(studentsarray[i].name + " : " + studentsarray[i].marks);
+        }
+
     }
 }
