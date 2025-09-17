9440cec09070e5a78728209026f6df6d1a784e0e
elbaheleng <elbaheleng@gmail.com>
2025-09-17 13:38:27 +0530

    inner class

---
+++ innerClass.java
@@ -0,0 +1,21 @@
+class A{
+    int age;
+    public void show(){
+        System.out.println("in show");
+    }
+    class B{
+        public void config(){
+            System.out.println("in config");
+        }
+    }
+}
+public class innerClass {
+    public static void main(String[] args) {
+        A obj = new A();
+        obj.show();
+
+        A.B obj1 = obj.new B();
+        obj1.config();
+
+    }
+}
