94a3516bbf258f10a81ce116310caa927fb26fcb
elbaheleng <elbaheleng@gmail.com>
2025-09-17 13:44:15 +0530

    anonymous inner class

--- innerClass.java
+++ innerClass.java
@@ -1,14 +1,17 @@
-class A{
+class A {
     int age;
-    public void show(){
+
+    public void show() {
         System.out.println("in show");
     }
-    class B{
-        public void config(){
+
+    class B {
+        public void config() {
             System.out.println("in config");
         }
     }
 }
+
 public class innerClass {
     public static void main(String[] args) {
         A obj = new A();
@@ -16,6 +19,13 @@ public class innerClass {
 
         A.B obj1 = obj.new B();
         obj1.config();
+
+        A obj2 = new A() { //anonymous inner class, this class is created inside Demo class and has no name
+            public void show() {
+                System.out.println("in new show");
+            }
+        };
+        obj2.show();
 
     }
 }
